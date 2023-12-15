1092e9262880da0d7311ab3617901fc2b607f00f
shimna <chimmu.p@gmail.com>
2023-12-15 20:35:15 +1100

    included mutation for login and addition of property, complaint and user

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -12,7 +12,7 @@ const resolvers = {
       try {
         return await Property.find(ownerId);
       } catch (error) {
-        console.log("Could not find properties");
+        console.log("Could not find properties", error);
       }
     },
     complaintsRaisedToAgent: async (parent, agentId) => {
@@ -21,7 +21,7 @@ const resolvers = {
           property: { $in: Property.find(agentId).select("_id") },
         });
       } catch (error) {
-        console.log("Could not find complaints");
+        console.log("Could not find complaints", error);
       }
     },
     complaintsOfPropertyByOwner: async (parent, ownerId) => {
@@ -30,7 +30,7 @@ const resolvers = {
           property: { $in: Property.find(ownerId) },
         });
       } catch (error) {
-        console.log("Could not find complaints");
+        console.log("Could not find complaints", error);
       }
     },
     complaintsRaisedByTenant: async (parent, tenantId) => {
@@ -39,9 +39,48 @@ const resolvers = {
           property: { $in: Property.find(tenantId) },
         });
       } catch {
-        console.log("Could not find complaints");
+        console.log("Could not find complaints", error);
       }
     },
   },
-  Mutation: {},
+  Mutation: {
+    addProperty: async (parent, propertyDetails) => {
+      try {
+        return Property.create(propertyDetails);
+      } catch (error) {
+        console.log("Could not add property!", error);
+      }
+    },
+    addComplaint: async (parent, { complaint, propertyId }) => {
+      try {
+        return Complaint.create({ complaint, propertyId });
+      } catch (error) {
+        console.log("Could not raise complaint", error);
+      }
+    },
+    addUser: async (parent, args) => {
+      try {
+        const user = await User.create(args);
+        return { user };
+      } catch (error) {
+        console.log("SignUp failed", error);
+      }
+    },
+    login: async ({ email, password }) => {
+      try {
+        const user = await User.findOne({ email });
+        if (!user) {
+          //   throw AuthenticationError;
+          console.log("Authentication failed", error);
+        }
+        const correctPassword = await user.isCorrectPassword(password);
+        if (!correctPassword) {
+          console.log("Authentication failed", error);
+        }
+        return { user };
+      } catch (error) {
+        console.log("Login failed", error);
+      }
+    },
+  },
 };
