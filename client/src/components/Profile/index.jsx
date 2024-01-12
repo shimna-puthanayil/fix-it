f7d7754cc8ce645b9188449f3e5cdbd64aa827e0
shimna <chimmu.p@gmail.com>
2024-01-12 11:58:09 +1100

    changed the path of importing complaint details

--- client/src/components/Profile/index.jsx
+++ client/src/components/Profile/index.jsx
@@ -9,7 +9,7 @@ import Link from "@mui/material/Link";
 import Navigator from "../../components/Profile/Navigator";
 import Content from "../../components/Profile/Content";
 import Header from "../../components/Profile/Header";
-import ComplaintDetails from "../Complaint/complaintDetails";
+import ComplaintDetails from "../../components/Complaint/ComplaintDetails";
 // import global state
 import { useComplaintContext } from "../../utils/GlobalState";
 function Copyright() {
