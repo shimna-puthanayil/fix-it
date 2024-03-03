064d7c7c891d746c0577492c79f3be1396bd321a
shimna <chimmu.p@gmail.com>
2024-03-03 19:08:30 +1100

    updated the image folder name

--- client/src/components/Complaint/AddComplaint/index.jsx
+++ client/src/components/Complaint/AddComplaint/index.jsx
@@ -203,7 +203,7 @@ export default function AddComplaint() {
   const formatFilename = (filename) => {
     const randomString = Math.random().toString(36).substring(2, 7);
     const cleanFileName = filename.toLowerCase().replace(/[^a-z0-9]/g, "-");
-    const newFilename = `images/${randomString}-${cleanFileName}`;
+    const newFilename = `complaint-images/${randomString}-${cleanFileName}`;
     return newFilename.substring(0, 60);
   };
   //removes attached/saved image from image list
